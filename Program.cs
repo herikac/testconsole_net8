724379b7f7c4c2aabe3de09c1937c115b6ce53ee
Herik <herik.100cia@gmail.com>
2024-06-04 19:25:56 -0500

    Comienzo del proyecto

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿string[] fraudulentOrderIDs = { "dsf", "sdfs", "sdfsdf" };
+/*fraudulentOrderIDs[0]="A422";
+fraudulentOrderIDs[1]="asdas21";
+fraudulentOrderIDs[2]="12";
+fr
+audulentOrderIDs[3]="sdfs1d2";
+*/
+Console.WriteLine(fraudulentOrderIDs.Length);
